944c2c571744bbb52c4da7be063269c2fa1bed09
justzxid <zaid.973@hotmail.com>
2025-01-13 00:32:48 +0300

    completed

--- script.js
+++ script.js
@@ -8,15 +8,15 @@ If they're equal, return either one.
 Exercise 1 has been completed for you:
 */
 
-// const maxOfTwoNumbers = (x, y) => {
-//     if (x >= y) {
-//       return x;
-//     } else {
-//       return y;
-//     }
-//   }
+const maxOfTwoNumbers = (x, y) => {
+    if (x >= y) {
+      return x;
+    } else {
+      return y;
+    }
+  }
   
-//   console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
+  console.log('Exercise 1 Result: ', maxOfTwoNumbers(3, 9));
 
   const isAdult = (age) => {
     if (age >= 18) {
@@ -28,16 +28,15 @@ Exercise 1 has been completed for you:
 
   console.log('Exercise 2 Result: ', isAdult(21));
 
-  const isCharAVowel = (char) => {
-    if (char === ('a')) {
-        return ('True');
-    } else {
-        return ('False')
-    }
+
+  const isCharAVowel = (x) => {
+    if (x === 'a' || x === 'e' || x === 'i' || x === 'o' || x ==='u') {
+      return true;
+  } else {
+      return false;
   }
-
-  console.log('Exercise 3 Result: ', isCharAVowel('a'));
-
+  }
+  console.log('Exercise 3 Result: ', isCharAVowel('h'));
 
 
   const generateEmail = (name, domain) => {
@@ -46,11 +45,16 @@ Exercise 1 has been completed for you:
 
   console.log('Exercise 4 Result: ', generateEmail("Johnsmith", "example.com"))
 
-  const greetUser = (name, timeOfDay) => {
-    return (`${timeOfDay}! ${name}`);
-  }
 
-  console.log('Exercise 5 Result: ', greetUser("Zaid", "Good morning"))
+  const morning = 'Good Morning!';
+  const evening = 'Good Evening!';
+  const afternoon = 'Good Afternoon!';
+  const userNames = ['Sam', 'John', 'Denis'];
+
+  function greetUser(timeOfDay, userNames) {
+  return `${timeOfDay}, ${userNames}`;
+}
+  console.log('Exercise 5 Result: ', greetUser(morning, userNames[1]));
 
   const maxOfThree = (num1, num2, num3) => {
     if (num1 > num2 || num1 > num3) {
@@ -62,11 +66,11 @@ Exercise 1 has been completed for you:
     } else ("Check your code");
   }
 
-    console.log('Exercise 6 Result: ', maxOfThree(50, 10, 8));
+  console.log('Exercise 6 Result: ', maxOfThree(50, 10, 8));
 
 
-  const calculateTip = (bill, perc) => {
-    return (bill * perc/100);
+  const calculateTip = (billAmount, tipPercentage) => {
+    return (billAmount * tipPercentage/100);
   }
 
   console.log('Exercise 7 Result: ', calculateTip(100,20))
@@ -75,12 +79,56 @@ Exercise 1 has been completed for you:
     if (scale === "C") {
       return (temperature * 9/5 + 32);
     } else if (scale === "F") {
-      return (temperature * 1.8 + 32);
+      return ((temperature - 32) * 5/9);
     }
   }
 
-  console.log('Exercise 8 Result: ', convertTemperature(100, "C"))
+  console.log('Exercise 8 Result: ', convertTemperature(89.6, "F"))
 
-  const basicCalculator = (num1, num2, ops) => {
+
+  const basicCalculator = (numberOne, numberTwo, operation) => {
+
+    if (operation === 'add') {
+      return numberOne + numberTwo;
+  } else if (operation === 'subtract') {
+    return numberOne - numberTwo;
+  } else if (operation === 'multiply') {
+    return numberOne * numberTwo;
+  } else if (operation === 'divide') {
+    return numberOne / numberTwo;
+  } else {
+    return ('Enter a valid number.')
+  }
 
   }
+
+  console.log('Exercise 9 Result: ',basicCalculator(10, 10, 'divide'));
+
+  const calculateGrade = (grade) => {
+      if (grade >= 90) {
+        return 'A'; 
+    } else if (grade <= 89 && grade >= 80) {
+        return 'B';
+    } else if (grade <= 79 && grade >= 70) {
+        return 'C';
+    } else if (grade <= 69 && grade >= 60) {
+        return 'D';
+    } else {
+        return 'F';
+    }
+  }
+
+  console.log('Exercise 10 Result:',calculateGrade(100));
+
+  const createUsername = (firstName, lastName) => {
+    const userName = firstName.slice(0, 3) + lastName.slice(0, 3) + (firstName.length + lastName.length);
+    return userName;
+  }
+
+  console.log('Exercise 11 Result:', createUsername("Samantha", "Green"));
+
+  const numArgs = () => {
+    return arguments.length - 1;
+}
+
+  console.log('Exercise 12 Result:', numArgs(1, 2, 3, 4));  
